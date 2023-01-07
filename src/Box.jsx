e0b8b337253d45686d10292f083b454645112c8c
sleeplesseditor <josh@bryson.me.uk>
2023-01-07 17:39:16 +0000

    State changes added

--- src/Box.jsx
+++ src/Box.jsx
@@ -3,16 +3,27 @@ import { useFrame } from '@react-three/fiber';
 
 export default function Box(props) {
     const ref = React.useRef();
+    const [hovered, setHover] = React.useState(false)
+    const [rotate, setRotate] = React.useState(false)
 
     useFrame((_, delta) => {
-        ref.current.rotation.x += 1 * delta
-        ref.current.rotation.y += 0.5 * delta
+        if(rotate){
+            ref.current.rotation.x += 1 * delta
+            ref.current.rotation.y += 0.5 * delta
+        }
     });
 
     return (
-        <mesh {...props} ref={ref}>
+        <mesh 
+            {...props}
+            onPointerDown={() => setRotate(!rotate)}
+            onPointerOver={() => setHover(true)}
+            onPointerOut={() => setHover(false)}
+            ref={ref}
+            scale={hovered ? [1.1, 1.1, 1.1] : [1, 1, 1]}
+        >
             <boxGeometry />
-            <meshBasicMaterial color={0x00ff00} wireframe />
+            <meshBasicMaterial color={hovered ? 0xff0000 : 0x00ff00} wireframe />
         </mesh>
     )
 }

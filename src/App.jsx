479f8bd48e2c7a0ba9ae1d95f1ac203f8f652414
Josh Bryson <josh@bryson.me.uk>
2023-01-11 10:49:24 +0000

    Lighting options added

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,33 @@ import Polyhedron from './Polyhedron';
 import * as THREE from 'three';
 import * as React from 'react';
 import { OrbitControls, Stats } from '@react-three/drei';
+import { Lights } from './Lights';
 
 export default function App() {
   return (
     <Canvas camera={{ position: [-1, 4, 2.5] }}>
-            <directionalLight position={[1, 1, 1]} />
+        <Lights />
         <Polyhedron
         name="meshBasicMaterial"
         position={[-3, 1, 0]}
-        material={new THREE.MeshBasicMaterial()}
+        material={new THREE.MeshBasicMaterial({ color: 'yellow', flatShading: true })}
       />
       <Polyhedron
         name="meshNormalMaterial"
         position={[-1, 1, 0]}
-        material={new THREE.MeshNormalMaterial()}
+        material={new THREE.MeshNormalMaterial({ flatShading: true })}
       />
       <Polyhedron
         name="meshPhongMaterial"
         position={[1, 1, 0]}
-        material={new THREE.MeshPhongMaterial()}
+        material={new THREE.MeshPhongMaterial({ color: 'lime', flatShading: true })}
       />
       <Polyhedron
         name="meshStandardMaterial"
         position={[3, 1, 0]}
-        material={new THREE.MeshStandardMaterial()}
+        material={new THREE.MeshStandardMaterial({ color: 0xff0033, flatShading: true })}
       />
-        <OrbitControls />
+        <OrbitControls target={[2, 2, 0]} />
         <axesHelper args={[5]}/>
         <gridHelper />
         <Stats />
--- src/Polyhedron.jsx
+++ src/Polyhedron.jsx
@@ -1,6 +1,4 @@
 import * as React from 'react';
-import { useControls } from 'leva';
-import * as THREE from 'three';
 import { useFrame } from '@react-three/fiber';
 
 export default function Polyhedron (props) {
@@ -10,33 +8,8 @@ export default function Polyhedron (props) {
         ref.current.rotation.y += 0.05 * delta   
     });
 
-    useControls((props.name), {
-        wireframe: {
-            value: false,
-            onChange: (v) => {
-              ref.current.material.wireframe = v
-            },
-        },
-        flatShading: {
-            value: true,
-            onChange: (v) => {
-              ref.current.material.flatShading = v
-              ref.current.material.needsUpdate = true
-            },
-        },
-        color: {
-            value: 'lime',
-            onChange: (v) => {
-              ref.current.material.color = new THREE.Color(v)
-            },
-        },
-    })
-
     return (
-        <mesh  
-            {...props}
-            ref={ref}
-        >
+        <mesh {...props} ref={ref}>
             <icosahedronGeometry args={[1, 1]} />
         </mesh>
     )

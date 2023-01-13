7f67e26f938f8db24ee3991facef0bb7f960aaf4
Josh Bryson <josh@bryson.me.uk>
2023-01-13 18:22:43 +0000

    Environment display added

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,21 @@
 
 import { Canvas, useLoader } from '@react-three/fiber';
 import * as React from 'react';
-import { Circle, OrbitControls, Stats } from '@react-three/drei';
+import { Environment, OrbitControls, Stats } from '@react-three/drei';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 export default function App() {
   const gltf = useLoader(GLTFLoader, './models/monkey.glb');
 
   return (
-    <Canvas camera={{ position: [-0.5, 1, 2] }} shadows>
-        <directionalLight castShadow position={[3.3, 1.0, 4.4]} />
+    <Canvas camera={{ position: [-0.5, 1, 2] }}>
+        <Environment background blur={0.5} files="./img/venice_sunset_4k.hdr" />
+        <directionalLight castShadow position={[3.3, 1.0, 4.4]} intensity={4} />
         <primitive
-          children-0-castShadow
           object={gltf.scene}
           position={[0, 1, 0]}
         />
-        <Circle args={[10]} rotation-x={-Math.PI / 2} receiveShadow>
-          <meshStandardMaterial />
-        </Circle>
-        <OrbitControls target={[0, 1, 0]} />
+        <OrbitControls target={[0, 1, 0]} autoRotate />
         <axesHelper args={[5]}/>
         <gridHelper />
         <Stats />

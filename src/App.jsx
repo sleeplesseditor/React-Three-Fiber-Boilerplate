7f24778f188ce1414bf22a77a1f267655387af1d
sleeplesseditor <josh@bryson.me.uk>
2023-02-05 20:57:08 +0000

    Shoe GLTFJSX example created

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,16 @@
 
 import { Canvas } from '@react-three/fiber';
 import * as React from 'react';
-import { Environment, OrbitControls, Stats } from '@react-three/drei';
-import { useControls } from 'leva';
-import { AnnotatedModel } from './AnnotatedModel';
-import Models from './models';
+import { Environment, OrbitControls, ContactShadows } from '@react-three/drei';
+import { Model } from './Shoe';
 
 export default function App() {
-  const { model } = useControls({
-    model: {
-      value: 'hammer',
-      options: Object.keys(Models),
-    }
-  });
-
   return (
-    <>
-      <Canvas camera={{ position: [0, 0, -0.2], near: 0.025 }}>
-        <Environment files="./img/workshop_1k.hdr" background />
-        <group>
-          <AnnotatedModel url={Models[model]} />
-        </group>
-        <OrbitControls autoRotate />
-        <Stats />
-      </Canvas>
-      <span id="info">The {model.replace(/([A-Z])/g, ' $1').toLowerCase()} is selected</span>
-    </>
+    <Canvas shadows camera={{ position: [0, 0, 1.66] }}>
+      <Environment preset="forest" />
+      <Model />
+      <ContactShadows position={[0, -0.8, 0]} color="#ffffff" />
+      <OrbitControls autoRotate />
+    </Canvas>
   )
 }
